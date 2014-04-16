0af15eb46b73acb966b1eb81080ed4846bb1a34b
Aaron Wilbee <ajwilbee713@hotmail.com>
2014-04-16 10:56:30 -0400

    deleted a bunch of things

--- OpenCVAxisCamera/test.cpp
+++ OpenCVAxisCamera/test.cpp
@@ -13,41 +13,41 @@ using namespace cv;
 
 int main(void)
 {
-	//VideoCapture videoCapture("http://axis1.student.rit.edu/mjpg/video.mjpg"); //http://axis1.student.rit.edu/mjpg/video.mjpg
-	//cvWaitKey(5000);
+	VideoCapture videoCapture("http://axis2.student.rit.edu/mjpg/video.mjpg"); //http://axis1.student.rit.edu/mjpg/video.mjpg
+	cvWaitKey(5000);
 
-	//if (!videoCapture.isOpened()){
+	if (!videoCapture.isOpened()){
 
-	//	int temp = 1;
-	//}
-	//Mat img;
-	//Mat hsv;
-	//Channel fourier = Channel();
-	//namedWindow("RAW", 1);
-	//namedWindow("HSV", 1);
-	//namedWindow("LayerOther", 1);
-	const char* filename = "opencv-logo-white.png";
-	Mat I = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-	Gabor test = Gabor(1, 0.5, 0, 8, 0); // the last one is theta
+		int temp = 1;
+	}
+	Mat img;
+	Mat hsv;
+	Channel fourier = Channel();
+	namedWindow("RAW", 1);
+	namedWindow("HSV", 1);
+	namedWindow("LayerOther", 1);
+
 	
-	imshow("test", I);
-	cout << "end";
-	CvGabor temp = CvGabor(1,.5,1);
-//	temp.conv_img();
-	/*while (waitKey(10)!='ESC')
+	while (waitKey(10)!='ESC')
 	{
 		videoCapture >> img;
 		bool temp =  img.empty();
 		
-		imshow("RAW", img);*/
-		/*cvtColor(img, hsv, CV_RGB2HSV);*/
 		
-		//fourier.setImage(img);
-	/*	imshow("RAW", fourier.getPyramid()[1]);
-		imshow("HSV", fourier.getPyramid()[2]);*/
-	//}
+		cvtColor(img, hsv, CV_RGB2HSV);
+		
 
+		imshow("RAW", img);
+		imshow("HSV", hsv);
+	}
 
+	//const char* filename = "opencv-logo-white.png";
+	//Mat I = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+	//Gabor test = Gabor(1, 0.5, 0, 8, 0); // the last one is theta
+
+	//imshow("test", I);
+	//cout << "end";
+	//CvGabor temp = CvGabor(1, .5, 1);
 
 	
 }
